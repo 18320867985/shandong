d21a1d47be20e8e5a4a26ecce66059e4543b03d1
18320867985 <18320867985>
2019-03-13 11:53:58 +0800

    2018-3-13

--- src/modules/index/index.js
+++ src/modules/index/index.js
@@ -1,8 +1,40 @@
 const index={
 	
 	init(){
-		console.log("index");
+	
+		lbt();
+		function lbt(){
+		
+			// 轮播图		
+			setCarouselLfBtn();
+	
+			$(window).resize(function() {
+				setCarouselLfBtn();
+			});
+	
+			function setCarouselLfBtn() {
+				if($(window).width() > 767) {
+					$(' .carousel.slide ').mouseenter(function() {
+	
+						$(this).find('.left.carousel-control,.right.carousel-control').stop().show();
+	
+					});
+	
+					$(' .carousel.slide').mouseleave(function() {
+	
+						$(this).find('.left.carousel-control,.right.carousel-control').stop().hide();
+					});
+				}else{
+					
+					$('.carousel.slide').find('.left.carousel-control,.right.carousel-control').hide();
+				}
+			}
+			
+			
+		}
 	}
+
+	
 }
 
 export{
