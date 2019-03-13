de2443770c384f2e7582bbf65e72dd70ee26c25e
18320867985 <18320867985>
2019-03-13 18:02:33 +0800

    2019-3-13

--- src/modules/index/index.js
+++ src/modules/index/index.js
@@ -30,6 +30,23 @@ const index={
 				}
 			}
 			
+			// 置顶
+			$(window).scroll(function(){
+				
+				if($(window).scrollTop()>500){
+					$(".scroll-top").show();
+				}else{
+					$(".scroll-top").hide();
+				}
+			});
+			
+			
+			//
+			$(".scroll-top").click(function(){
+				$("body,html").animate({
+					scrollTop:0
+				},500);
+			});
 			
 		}
 	}
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,1 +1,119 @@
-!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?e(exports):"function"==typeof define&&define.amd?define(["exports"],e):e(t.umd={})}(this,function(t){!function(){function l(t,e){var o=e.split("."),n="",i="";0<=o.length&&(n=(n=o[0]||"").trim()),1===o.length?i="init":2===o.length&&(i=o[1].trim());var r=window[t];if(r&&r[n]){var l=r[n][i];"function"==typeof l&&l()}}window.onload=function(){for(var t=document.querySelectorAll("script[data-umd]"),e=0;e<t.length;e++){var o="umd",n=t[e];if(n){o=n.getAttribute("data-parent")||o;var i=(n.getAttribute("data-umd")||"").split(/,|;|&/);for(e=0;e<i.length;e++){var r=i[e]||"";""!==r.trim()&&l(o,r.trim())}}}}}();var e={init:function(){!function(){function t(){767<$(window).width()?($(" .carousel.slide ").mouseenter(function(){$(this).find(".left.carousel-control,.right.carousel-control").stop().show()}),$(" .carousel.slide").mouseleave(function(){$(this).find(".left.carousel-control,.right.carousel-control").stop().hide()})):$(".carousel.slide").find(".left.carousel-control,.right.carousel-control").hide()}t(),$(window).resize(function(){t()})}()}};t.index=e,Object.defineProperty(t,"__esModule",{value:!0})});
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.umd = {})));
+}(this, (function (exports) {
+
+/*自动执行 rollup打包umd格式js模块
+	 
+	 * <script   data-parent="umd" data-umd="test" src="./js/all.js" type="text/javascript" charset="utf-8"></script>
+	 * */
+
+(function () {
+	window.onload = function () {
+		var el_umds = document.querySelectorAll("script[data-umd]");
+		for (var i = 0; i < el_umds.length; i++) {
+
+			var parent = "umd";
+			var el_umd = el_umds[i];
+			if (el_umd) {
+
+				parent = el_umd.getAttribute("data-parent") || parent;
+				var umd_str = el_umd.getAttribute("data-umd") || "";
+				var umd_strs = umd_str.split(/,|;|&/);
+				for (var i = 0; i < umd_strs.length; i++) {
+					var item = umd_strs[i] || "";
+					if (item.trim() !== "") {
+						setUmd(parent, item.trim());
+					}
+				}
+			}
+		}
+	};
+
+	function setUmd(parent, umd_str) {
+
+		var arrs = umd_str.split(".");
+		var prop1 = "";
+		var prop2 = "";
+		if (arrs.length >= 0) {
+			prop1 = arrs[0] || "";
+			prop1 = prop1.trim();
+		}
+
+		if (arrs.length === 1) {
+			prop2 = "init";
+		} else if (arrs.length === 2) {
+			prop2 = arrs[1].trim();
+		}
+
+		var obj = window[parent];
+		if (!obj) {
+			return;
+		}
+		if (!obj[prop1]) {
+			return;
+		}
+		var fn = obj[prop1][prop2];
+
+		if (typeof fn === "function") {
+			fn();
+		}
+	}
+})();
+
+var index = {
+	init: function init() {
+
+		lbt();
+		function lbt() {
+
+			// 轮播图		
+			setCarouselLfBtn();
+
+			$(window).resize(function () {
+				setCarouselLfBtn();
+			});
+
+			function setCarouselLfBtn() {
+				if ($(window).width() > 767) {
+					$(' .carousel.slide ').mouseenter(function () {
+
+						$(this).find('.left.carousel-control,.right.carousel-control').stop().show();
+					});
+
+					$(' .carousel.slide').mouseleave(function () {
+
+						$(this).find('.left.carousel-control,.right.carousel-control').stop().hide();
+					});
+				} else {
+
+					$('.carousel.slide').find('.left.carousel-control,.right.carousel-control').hide();
+				}
+			}
+
+			// 置顶
+			$(window).scroll(function () {
+
+				if ($(window).scrollTop() > 500) {
+					$(".scroll-top").show();
+				} else {
+					$(".scroll-top").hide();
+				}
+			});
+
+			//
+			$(".scroll-top").click(function () {
+				$("body,html").animate({
+					scrollTop: 0
+				}, 500);
+			});
+		}
+	}
+};
+
+exports.index = index;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
